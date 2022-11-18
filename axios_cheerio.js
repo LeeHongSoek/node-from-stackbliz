f1da4bb58f26871a1dbb15d7ed5e76af884622b0
LeeHongSoek <105229755+LeeHongSoek@users.noreply.github.com>
2022-11-18 22:59:19 +0900

    연합뉴스 스포츠 크롤링하기...

--- axios_cheerio.js
+++ axios_cheerio.js
@@ -1,9 +1,16 @@
+/**
+ * https://velog.io/@yesdoing/Node.js-에서-웹-크롤링하기-wtjugync1m
+ *
+ * Node.js 에서 웹 크롤링하기    # npm install axios cheerio
+ *
+ */
 const axios = require('axios');
 const cheerio = require('cheerio');
 const log = console.log;
 
 const getHtml = async () => {
   try {
+    // 연합뉴스 스포츠
     return await axios.get('https://www.yna.co.kr/sports/all');
   } catch (error) {
     console.error(error);
@@ -14,16 +21,15 @@ getHtml()
   .then((html) => {
     let ulList = [];
     const $ = cheerio.load(html.data);
-    const $bodyList = $('div.headline-list ul').children('li.section02');
+    const $bodyList = $('div.list-type038 ul').children('li');
 
     $bodyList.each(function (i, elem) {
       ulList[i] = {
-        title: $(this).find('strong.news-tl a').text(),
-        url: $(this).find('strong.news-tl a').attr('href'),
-        image_url: $(this).find('p.poto a img').attr('src'),
-        image_alt: $(this).find('p.poto a img').attr('alt'),
-        summary: $(this).find('p.lead').text().slice(0, -11),
-        date: $(this).find('span.p-time').text(),
+        date: $(this).find('span.txt-time').text(),
+        title: $(this).find('strong.tit-news').text(),
+        url: $(this).find('a.tit-wrap').attr('href'),
+        image_url: $(this).find('a.img img').attr('src'),
+        image_alt: $(this).find('a.img img').attr('alt'),
       };
     });
 
