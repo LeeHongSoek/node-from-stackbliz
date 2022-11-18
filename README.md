da8707860d31588c48f7f48b8a4c8f42f135f806
LeeHongSoek <105229755+LeeHongSoek@users.noreply.github.com>
2022-11-18 22:09:15 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# node-from-stackbliz
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/node-c7uytj)
---
+++ index.js
@@ -0,0 +1,3 @@
+// run `node index.js` in the terminal
+
+console.log(`안녕~! Node.js v${process.versions.node}!`);
